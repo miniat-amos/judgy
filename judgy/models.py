ee8dc4544792e60a65c47e9df41934bc04661536
Sazzad Ibrahim <ibrahis@sunypoly.edu>
2024-09-25 23:45:00 -0400

    rename id to code

--- judgy/models.py
+++ judgy/models.py
@@ -19,7 +19,7 @@ class User(AbstractUser):
         return self.email
 
 class Competition(models.Model):
-    id = models.CharField(editable=False, max_length=4, primary_key=True)
+    code = models.CharField(editable=False, max_length=4, primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
     start = models.DateTimeField()
@@ -28,11 +28,11 @@ class Competition(models.Model):
     enroll_end = models.DateTimeField()
 
     def save(self, *args, **kwargs):
-        if not self.id:
+        if not self.code:
             while True:
-                new_id = uuid.uuid4().hex[:4].upper()
-                if not Competition.objects.filter(id=new_id).exists():
-                    self.id = new_id
+                code = uuid.uuid4().hex[:4].upper()
+                if not Competition.objects.filter(code=code).exists():
+                    self.code = code
                     break
         super().save(*args, **kwargs)
 
