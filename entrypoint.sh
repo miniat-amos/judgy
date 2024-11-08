3fb5fabbe03ce5666bfef696401380d99dd50118
Slava <yaroslavborysyuk1@gmail.com>
2024-11-08 13:25:43 -0500

    Create super user when django server starts up

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 echo "Entrypoint script is running"
 
-
 until mysqladmin ping -h "judgy_mysql_prod" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --silent; do
     echo "Waiting for MySQL..."
     sleep 2
@@ -12,6 +11,6 @@ python manage.py makemigrations judgy --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-# DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
+DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
 
 gunicorn progcomp.wsgi:application --bind 0.0.0.0:8000
