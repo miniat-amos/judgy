d65ffa53f7219ce4eac5d573aa62064008ae4ae6
Slava <borysyy@sunypoly.edu>
2024-11-15 18:07:38 -0500

    Fixed pinging system

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ echo "Starting mysql container"
 
 docker compose -f ./docker-compose.web.yml up --build db -d 
 
-until mysqladmin ping -h "127.0.0.1" -P 3306 -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --silent; do
+until mysqladmin ping -h "127.0.0.1" -P "${MYSQL_PORT}" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --silent; do
     echo "Waiting for MySQL..."
     sleep 2
 done
