292ea30754dce5e9c6b6e89aa04a9a24126649c1
Slava <borysyy@sunypoly.edu>
2024-11-16 15:12:37 -0500

    Added echo statements

--- django_project/restart_django.sh
+++ django_project/restart_django.sh
@@ -8,8 +8,10 @@ restart_server() {
     nohup gunicorn progcomp.wsgi:application --bind 0.0.0.0:8000 --workers 17 > logs/server_$(date +%F_%T).log 2>&1 &
 }
 
+echo "Activating python virtual env"
 source ./env/bin/activate
 
+echo "Entering django project directory"
 cd ./django_project
 
 restart_server
