6b8510d603204c7a4f3e5b931bb78ac8471060d8
Slava <borysyy@sunypoly.edu>
2024-11-18 18:27:53 -0500

    Fixed restart django script

--- django_project/restart_django.sh
+++ django_project/restart_django.sh
@@ -4,7 +4,7 @@
 restart_server() {
     echo "Restart Gunicorn server"
     pkill gunicorn || true  # Kill any existing Gunicorn processes
-    mkdir ./logs
+    mkdir -p ./logs
     nohup gunicorn progcomp.wsgi:application --bind 0.0.0.0:8000 --workers 17 > logs/server_$(date +%F_%T).log 2>&1 &
 }
 
