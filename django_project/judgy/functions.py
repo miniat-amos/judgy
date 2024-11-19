a08b024a604f9dc0909d20c66c15c27d58d19eab
frankandlouie <louief@sunypoly.edu>
2024-11-18 21:05:21 -0500

    Java support added; Multiple files (extra file class support) works.

--- django_project/judgy/functions.py
+++ django_project/judgy/functions.py
@@ -5,13 +5,15 @@ from .utils import make_file, create_user_dir
 from django.conf import settings
 from pathlib import Path
 
+from docker.errors import ContainerError
+
 languages = {
     ".py": {"image": "python", "type": "interpreted", "interpreter": "python3"},
     ".js": {"image": "node", "type": "interpreted", "interpreter": "node"},
     ".rb": {"image": "ruby", "type": "interpreted", "interpreter": "ruby"},
     ".c": {"image": "gcc", "type": "compiled", "compiler": "gcc"},
     ".cpp": {"image": "gcc", "type": "compiled", "compiler": "g++"},
-    ".java": {"image": "openjdk", "type": "compiled-and-interpreted", "compiler": "javac", "interpreter": "java",
+    ".java": {"image": "java", "type": "compiled-and-interpreted", "compiler": "javac", "interpreter": "java",
 }
 }
 
@@ -41,15 +43,10 @@ def start_containers(f, current_user):
     output_file = make_file(output_dir, "output.txt")
     score_file = make_file(output_dir, "score.txt")
 
-    # Connect to docker daemon
-    try:    
-        client = docker.from_env()
-        print("docker initialized")
-    except Exception as e:
-        print(f"Error initializing Docker client: {e}")
+    client = docker.from_env()
 
     # Variables for container
-    docker_image = f"judgy-0012-{submitted_image}_app"
+    docker_image = f"judgy-92f5-{submitted_image}_app"
     container_name = f"{submitted_image}_{current_user.first_name}_container"
     container_main_directory = Path("/app")
     # container_user_file = container_main_directory / f.name
@@ -67,8 +64,8 @@ def start_containers(f, current_user):
 
     # Debugging prints
     # print(f"Submitted file path: {submitted_file}")
-    print(f"Output file path: {output_file}")
-    print(f"Score file path: {score_file}")
+    # print(f"Output file path: {output_file}")
+    # print(f"Score file path: {score_file}")
 
      # Volumes for file-to-file binding
     volumes = {
@@ -81,7 +78,7 @@ def start_containers(f, current_user):
     for submitted_file in submitted_files:
         # file_extension = os.path.splitext(submitted_file.name)[1]
         submitted_image = languages[file_extension]["image"]
-        docker_image = f"judgy-ed68-{submitted_image}_app"
+        docker_image = f"judgy-92f5-{submitted_image}_app"
         container_name = f"{submitted_image}_container"
         container_main_directory = Path("/usr/app")
         container_user_file = container_main_directory / submitted_file.name
@@ -92,13 +89,31 @@ def start_containers(f, current_user):
         # Directory where Java files are stored in the container
         interpreter = languages[file_extension]["interpreter"]
         compiler = languages[file_extension]["compiler"]
+            
+        def find_main_file():
+            for f in submitted_files:
+                with open(f, 'r') as file:
+                    content = file.read()
+                    if 'public static void main(String[] args)' in content:
+                        container_main_path = Path("/usr/app") / f.name
+                        return container_main_path
+            return None
         
+        def classes_string():
+            file_paths = []
+            for f in submitted_files:
+                file_path = Path("/usr/app") / f.name
+                file_paths.append(str(file_path))
+            return " ".join(file_paths)
+
+        main_file = find_main_file()
+        classes = classes_string()
+        classpath = "/usr/app/"
+
         # Command to compile and execute the Java files
         container = client.containers.run(
             docker_image,
-            command=f"""
-                bash -c '{compiler} Main.java {interpreter} Main.class {container_output_path}'
-                """,
+            command=f'bash -c "{compiler} {classes} && {interpreter} -cp {classpath} {main_file.stem} < /app/golddigger-mine.dat > {container_output_path} && python3 golddigger-judge.py golddigger-mine.dat {compiler} {classes} && {interpreter} -cp {classpath} {main_file.stem} > {container_score_path}"',
             volumes=volumes,
             detach=True,
             name=container_name,
@@ -114,7 +129,6 @@ def start_containers(f, current_user):
             name=container_name,
         )
     elif languages[file_extension]["type"] == "compiled":
-        print("I MADE IT TO ELIF COMPILED LANGAUGES")
         compiler = languages[file_extension]["compiler"]
         container = client.containers.run(
             docker_image,
@@ -128,7 +142,6 @@ def start_containers(f, current_user):
     container.remove()
 
     return output_file, score_file
-
 
 # Create Docker images based on the competition code
 # Preloads all Docker images with problem files
