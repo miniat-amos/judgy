c65319399bd180b813621a5248c3804cbbd8310a
Slava <borysyy@sunypoly.edu>
2024-11-22 00:06:59 -0500

    Increased runtime for gunicorn processes

--- django_project/entrypoint.sh
+++ django_project/entrypoint.sh
@@ -69,7 +69,7 @@ start_server() {
     echo "Starting Gunicorn server"
     pkill gunicorn || true  # Kill any existing Gunicorn processes
     mkdir -p ./logs
-    nohup gunicorn --timeout 150 progcomp.wsgi:application --bind 0.0.0.0:8000 --workers 17 > logs/server_$(date +%F_%T).log 2>&1 &
+    nohup gunicorn --timeout 300 progcomp.wsgi:application --bind 0.0.0.0:8000 --workers 17 > logs/server_$(date +%F_%T).log 2>&1 &
 }
 
 # Run virtual env function
