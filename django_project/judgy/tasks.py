ca1a78f6794cc7f6516ae06438826bc743992a18
Slava Borysyuk <yaroslavborysyuk1@gmail.com>
2025-04-15 02:31:08 -0400

    Cleaned up imports in the celery file

--- django_project/judgy/tasks.py
+++ django_project/judgy/tasks.py
@@ -1,12 +1,12 @@
+import subprocess
 from celery import shared_task
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
 from django.contrib.auth import get_user_model
-
-import subprocess
-from pathlib import Path
 from django.conf import settings
+
+from pathlib import Path
 
 @shared_task
 def send_6dc_email_task(user_id, user_email):
