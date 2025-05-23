9b78bd9356a170ce45622f2a8184c79b9fcd8704
Slava Borysyuk <yaroslavborysyuk1@gmail.com>
2025-05-23 16:04:08 -0400

    Needed to reverse urls.py back to the old version

--- django_project/judgy/urls.py
+++ django_project/judgy/urls.py
@@ -34,50 +34,33 @@ from .views import (
 app_name = 'judgy'
 
 urlpatterns = [
-    # Home page
     path('', home_view, name='home'),
-    
-    # User related urls
     path('login', login_view, name='login'),
     path('logout', logout_view, name='logout'),
     path('register', register_view, name='register'),
     path('verify', verify_view, name='verify'),
     path('forgot-password', forgot_password_view, name='forgot_password'),
     path('reset-password/<uidb64>/<token>', reset_password_view, name='reset_password'),
-    
-    # Notification urls
     path('notifications', notifications_view, name='notifications'),
     path('notification/<str:id>/clear', notification_clear_view, name='notification_clear'),
     path('set-timezone', set_timezone_view, name='set_timezone'),
-    
-    # Getting competition urls
-    path('competitions', competitions_view, name='competitions'),
     path('all-competitions', see_competitions_view, name='see_competitions'),
-    path('competition/<str:code>', competition_code_view, name='competition_code'),
-    
-    # Admin specific competitions urls
     path('competition/create', competition_create_view, name='competition_create'),
     path('competition/<str:code>/problems/update', problems_update_view, name='problems_update'),
-    path('competition/<str:code>/update', CompUpdate.as_view(), name="update_competition"),
-    
-    # Team action Urls
-    path('competition/<str:code>/team/<str:name>', team_name_view, name='team_name'),
+    path('competition/<str:code>', competition_code_view, name='competition_code'),
     path('competition/<str:code>/team/enroll', team_enroll_view, name='team_enroll'),
-    path('competition/<str:code>/team/invite', team_invite_view, name='team_invite'),
-    path('competition/<str:code>/team/leave', team_leave_view, name='team_leave'),
-    
-    # Team join/invite urls
     path('team/join/<str:id>/accept', team_join_accept_view, name='team_join_accept'),
     path('team/join/<str:id>/decline', team_join_decline_view, name='team_join_decline'),
+    path('competition/<str:code>/team/leave', team_leave_view, name='team_leave'),
+    path('competition/<str:code>/team/invite', team_invite_view, name='team_invite'),
     path('team/invite/<str:id>/accept', team_invite_accept_view, name='team_invite_accept'),
     path('team/invite/<str:id>/decline', team_invite_decline_view, name='team_invite_decline'),
-    
-    # Problem urls
+    path('competition/<str:code>/team/<str:name>', team_name_view, name='team_name'),
+    path('competitions', competitions_view, name='competitions'),
     path('competition/<str:code>/<str:problem_name>/download', download_view, name='download'),
     path('competition/<str:code>/<str:problem_name>/submit', submit_view, name='submit'),
-    path('competition/<str:code>/<str:problem_name>/submission/output', output_view, name='output'),
-    
-    # Competition specific urls 
     path('competition/<str:code>/rankings', rankings_view, name='rankings'),
+    path('competition/<str:code>/<str:problem_name>/submission/output', output_view, name='output'),
     path('competition/<str:code>/team/<str:name>/members', get_members_view, name='team_members'),
+    path('competition/<str:code>/update', CompUpdate.as_view(), name="update_competition"),
 ]
