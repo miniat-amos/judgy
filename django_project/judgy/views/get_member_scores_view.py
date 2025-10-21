1e8b7b665eac4f050205d85d9fdbe53dd74ac0aa
Slava Borysyuk <yaroslavborysyuk1@gmail.com>
2025-10-20 23:16:12 -0400

    New views to get members and one to get their scores

---
+++ django_project/judgy/views/get_member_scores_view.py
@@ -0,0 +1,37 @@
+from django.db.models import Min, Max
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from ..models import Competition, Team, Problem, Submission
+
+
+def get_member_scores(request, code, name):
+    competition = get_object_or_404(Competition, code=code)
+    team = get_object_or_404(Team, competition=competition, name=name)
+    members = team.members.all().values('id', 'email', 'first_name')
+    problems = Problem.objects.filter(competition=competition).order_by('number')
+        
+    member_scores = {
+        'team': team.name,
+        'members': {}
+    }
+    
+    for member in members:
+        email = member['email']
+        member_scores['members'][email] = {
+            'first_name': member['first_name'],
+            'scores': {}
+        }
+
+        for problem in problems:
+            user_submissions = Submission.objects.filter(problem=problem, team=team, user=member['id'])
+            if problem.score_preference: # Higher Score is Better
+                user_best_score = user_submissions.aggregate(Max('score'))['score__max']
+            else: # Lower Score is Better
+                user_best_score = user_submissions.aggregate(Max('score'))['score__min']
+                
+            member_scores['members'][email]['scores'][problem.name] = {
+                "member_score": str(user_best_score) if user_best_score is not None else "",
+                "subjective": problem.subjective
+            }
+
+    return JsonResponse(member_scores, safe=False)
--- django_project/judgy/views/get_members_view.py
+++ django_project/judgy/views/get_members_view.py
@@ -5,5 +5,5 @@ from ..models import Competition, Team
 def get_members_view(request, code, name):
     competition = get_object_or_404(Competition, code=code)
     team = get_object_or_404(Team, competition=competition, name=name)
-
-    return JsonResponse(list(team.members.all().values()), safe=False)
+    
+    return JsonResponse(list(team.members.all().values('email', 'first_name')), safe=False)
