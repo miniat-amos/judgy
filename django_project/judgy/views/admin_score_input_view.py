5a873b1a8f2a704dc43622cf200569514ef0fe6c
Slava Borysyuk <yaroslavborysyuk1@gmail.com>
2025-10-22 22:44:33 -0400

    API endpoint to update submission score

--- django_project/judgy/views/admin_score_input_view.py
+++ django_project/judgy/views/admin_score_input_view.py
@@ -1,25 +1,12 @@
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
-from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
-from ..models import Submission
-from ..serializers import CompSerializer
+from ..models import Submission, Competition
+from ..serializers import SubmissionScoreSerializer
 
-# Class for updating a competition
-class ScoreUpdate(APIView):
-    def put(self, request, submission):
-        
-        
-        competition = get_object_or_404(Competition, code=code)
-        
-        serializer = CompSerializer(competition, data=request.data, partial=True)
-        
-        if serializer.is_valid():
-            serializer.save()
-            
-            redirect_url = reverse("judgy:competition_code", kwargs={"code":code})
-            return Response({"success": f"Competition {competition.name} updated successfully!", "redirect_url": redirect_url}, status=status.HTTP_200_OK)
-        else:
-            print(serializer.errors)
-            return Response({"errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+# Class for updating a score as an Admin
+class ScoreUpdate(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Submission.objects.all()
+    serializer_class = SubmissionScoreSerializer
