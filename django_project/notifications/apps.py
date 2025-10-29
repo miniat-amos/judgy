b5d17dbf556943ae9085fce92233000fb56715ff
Slava Borysyuk <yaroslavborysyuk1@gmail.com>
2025-10-29 18:58:23 -0400

    New Notifications app to handle websocket notifications

---
+++ django_project/notifications/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+class NotificationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'notifications'
+
+    def ready(self):
+        from notifications import signals
---
+++ django_project/notifications/signals.py
@@ -0,0 +1,16 @@
+# notifications/signals.py
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from judgy.models import Notification
+from .utils import send_notification
+
+@receiver(post_save, sender=Notification)
+def push_notification(sender, instance, created, **kwargs):
+    if created:
+        data = {
+            "id": instance.id,
+            "type": instance.type,
+            "header": instance.header,
+            "body": instance.body,
+        }
+        send_notification(instance.user, data)
