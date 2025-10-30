065739a46182bd96f53fdea60691ebcfe186d84c
Slava Borysyuk <yaroslavborysyuk1@gmail.com>
2025-10-30 18:59:37 -0400

    Moved Notification specific views over to the Notifications app.

--- django_project/notifications/views/notification_clear_view.py
+++ django_project/notifications/views/notification_clear_view.py
@@ -1,9 +1,7 @@
-
-
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-from ..decorators import verified_required
-from ..models import Notification
+from judgy.decorators import verified_required
+from notifications.models import Notification
 
 @verified_required
 def notification_clear_view(request, id):
