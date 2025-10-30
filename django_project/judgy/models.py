e163a4cde25e609b122ca4b1140b9197d3a6d163
Slava Borysyuk <yaroslavborysyuk1@gmail.com>
2025-10-30 18:58:05 -0400

    Moved all Notification models to the Notifications app. Notifications are now being handled exclusively inside of the Notifications app.

--- django_project/judgy/models.py
+++ django_project/judgy/models.py
@@ -92,37 +92,4 @@ class Submission(models.Model):
     score = models.BigIntegerField(blank=True, null=True)
     time = models.DateTimeField(auto_now_add=True)
 
-class Notification(models.Model):
-    type = models.IntegerField(default=0)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    header = models.CharField(max_length=255)
-    body = models.TextField()
 
-    def __str__(self):
-        return self.header
-
-class TeamJoinNotification(Notification):
-    request_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
-
-    def save(self, *args, **kwargs):
-        self.type = 1
-        self.header = 'Join Request'
-        TeamJoinNotification.objects.filter(
-            user=self.user,
-            request_user=self.request_user,
-            team=self.team
-        ).delete()
-        super().save(*args, **kwargs)
-
-class TeamInviteNotification(Notification):
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
-
-    def save(self, *args, **kwargs):
-        self.type = 1
-        self.header = 'Team Invite'
-        TeamInviteNotification.objects.filter(
-            user=self.user,
-            team=self.team
-        ).delete()
-        super().save(*args, **kwargs)
