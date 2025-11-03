95fe5261ee392d5a18d54ec97a42d1aaf2026cd4
Slava Borysyuk <yaroslavborysyuk1@gmail.com>
2025-11-03 16:56:29 -0500

    No more error when disconnecting websocket if user is not authenticated

--- django_project/notifications/consumers.py
+++ django_project/notifications/consumers.py
@@ -12,8 +12,8 @@ class NotificationConsumer(AsyncWebsocketConsumer):
             await self.close()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+        if hasattr(self, "group_name"):
+            await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def notify(self, event):
-        # event = {"type": "notify", "data": {...}}
         await self.send(text_data=json.dumps(event["data"]))
