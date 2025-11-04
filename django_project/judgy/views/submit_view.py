925018e29f1692bcfbae5db9494f7c88ed54660e
frankandlouie <louief@sunypoly.edu>
2025-11-03 19:29:49 -0500

    Reworked tasks submit_view and functions to make running contianers a background process.

--- django_project/judgy/views/submit_view.py
+++ django_project/judgy/views/submit_view.py
@@ -1,8 +1,9 @@
 import math
+import os
+from django.conf import settings
 from django.db.models import Min, Max
 from django.shortcuts import redirect, get_object_or_404
 from django.utils import timezone
-from django.utils.html import format_html
 from ..decorators import verified_required
 from ..forms import SubmissionForm
 from ..models import (
@@ -14,6 +15,7 @@ from ..models import (
     User
 )
 from ..functions import run_submission
+from ..tasks import process_submission
 
 @verified_required
 def submit_view(request, code, problem_name):
@@ -26,60 +28,20 @@ def submit_view(request, code, problem_name):
             form = SubmissionForm(request.POST, request.FILES)
             if form.is_valid():
                 files = request.FILES.getlist('files')
-                score_file, output_file, language, file_name = run_submission(code, problem, user_team, request.user, files)
-                request.session['output_dir'] = str(output_file)
-                
-                with open(score_file, 'r') as f:
-                    score = f.read()
-                score = score.split(' ')[0]
-                
-                with open (output_file, 'r') as f:
-                    file_output = f.read()
 
-                if problem.show_output:
-                    output_url = f'/competition/{code}/{problem_name}/submission/output'
-                    body = format_html(
-                        'You got a score of {} in the problem "{}" for the competition "{}".<br>'
-                        'Click <a href="{}" target="_blank">here</a> to see the output.',
-                        score,
-                        problem.name,
-                        competition.name,
-                        output_url
-                    )
-                else:
-                    body=f'You got a score of {score} in the problem "{problem.name}" for the competition "{competition.name}".'
+                file_paths = []
+                save_dir = os.path.join(settings.MEDIA_ROOT, 'temp_submissions')
+                os.makedirs(save_dir, exist_ok=True)
+                for f in files:
+                    save_path = os.path.join(save_dir, f.name)
+                    with open(save_path, 'wb+') as dest:
+                        for chunk in f.chunks():
+                            dest.write(chunk)
+                    file_paths.append(save_path)
 
-                Notification.objects.create(
-                    user=request.user,
-                    header='Your Score',
-                    body=body,
-                )
+                print(type(file_paths), file_paths)
 
-                competition_submissions = Submission.objects.filter(problem=problem)
-                if problem.score_preference: # Higher Score is Better
-                    competition_best_score = competition_submissions.aggregate(Max('score'))['score__max'] or -math.inf
-                    if int(score) > competition_best_score:
-                        superusers = User.objects.filter(is_superuser=True)
-                        participants = User.objects.filter(teams__competition=competition)
-                        header = 'New Best Score'
-                        body = f'{request.user.first_name} from team "{user_team}" has achieved a new best score of {score} in the problem "{problem.name}" for the competition "{competition.name}"!'
-                        for user in superusers:
-                            Notification.objects.create(user=user, header=header, body=body)
-                        for user in participants:
-                            Notification.objects.create(user=user, header=header, body=body)
-                else: # Lower Score is Better
-                    competition_best_score = competition_submissions.aggregate(Min('score'))['score__min'] or +math.inf
-                    if int(score) < competition_best_score:
-                        superusers = User.objects.filter(is_superuser=True)
-                        participants = User.objects.filter(teams__competition=competition)
-                        header = 'New Best Score',
-                        body = f'{request.user.first_name} from team "{user_team}" has achieved a new best score of {score} in the problem "{problem.name}" for the competition "{competition.name}"!'
-                        for user in superusers:
-                            Notification.objects.create(user=user, header=header, body=body)
-                        for user in participants:
-                            Notification.objects.create(user=user, header=header, body=body)
-
-                Submission.objects.create(problem=problem, team=user_team, user=request.user, language=language, file_name=file_name, output=file_output, score=score)
+                process_submission.delay(code, competition.code, problem.id, problem_name, user_team.id, request.user.id, file_paths)
 
                 return redirect('judgy:competition_code', code=code)
             else:
