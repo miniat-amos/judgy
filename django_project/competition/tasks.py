a9843bb5bf3d103becd348b1c9e7e7e5e27bcf36
Slava <yaroslavborysyuk1@gmail.com>
2026-04-22 19:58:37 -0400

    refactor: Updated the celery tasks for the comp
    - New task for creating images
    - Slight updated handling of objective and subjective problem submissions

--- django_project/competition/tasks.py
+++ django_project/competition/tasks.py
@@ -5,8 +5,8 @@ from django.urls import reverse
 from django.utils.html import format_html
 from pathlib import Path
 from judgy.models import User
-from competition.functions import run_submission
-from competition.utils import notify_admin_submission, create_user_dir, store_submissions, check_competition_best
+from competition.docker_functions import create_images, run_submission
+from competition.utils import notify_admin_submission, check_competition_best
 from competition.models import (
     Competition,
     Problem,
@@ -16,34 +16,35 @@ from competition.models import (
 from notifications.models import Notification
 
 @shared_task
-def create_images_task(competition_code):
-    docker_image_script = Path(settings.BASE_DIR) / "docker_setup.sh"
-    subprocess.run(f"bash {docker_image_script} {competition_code.lower()}", shell=True)
+def celery_build_judgy_images():
+    create_images.build_judgy_images()
+
 
 @shared_task
-def process_submission(code, problem, team, user, file_paths):
+def celery_process_submission(code, problem, team, user, submission, user_directories, user_submission):
     competition = Competition.objects.get(code=code)
     problem = Problem.objects.get(id=problem)
     user_team = Team.objects.get(id=team)
     user = User.objects.get(id=user)
+    submission = Submission.objects.get(id=submission)
     
     if not problem.subjective:
-        
-        score_file, output_file, language, file_name = run_submission(code, problem, user_team, user, file_paths)
+    
+        score_file, output_file, language, file_name = run_submission.run_submission(code, user, problem, user_submission, user_directories)
 
         with open(score_file, 'r') as f:
-            score = f.read().split(' ')[0]
+            submission_score = int(f.read().split(' ')[0])
         with open(output_file, 'r') as f:
-            file_output = f.read()
-     
+            submission_output = f.read()
+        
         if problem.show_output:
             output_url = reverse('competition:output', kwargs={'code': code, 'problem_name': problem.name})
             body = format_html(
-                f'You got a score of {score} in the problem "{problem.name}" for the competition "{competition.name}".<br>'
+                f'You got a score of {submission_score} in the problem "{problem.name}" for the competition "{competition.name}".<br>'
                 f'Click <a href="{output_url}" target="_blank">here</a> to see the output.',
             )
         else:
-            body=f'You got a score of {score} in the problem "{problem.name}" for the competition "{competition.name}".'
+            body=f'You got a score of {submission_score} in the problem "{problem.name}" for the competition "{competition.name}".'
 
         Notification.objects.create(
             user=user,
@@ -51,40 +52,25 @@ def process_submission(code, problem, team, user, file_paths):
             body=body,
         )
 
-        check_competition_best(competition, problem, score, user, user_team)
         
-        Submission.objects.create(
-            problem=problem,
-            team=user_team,
-            user=user,
-            language=language,
-            file_name=file_name,
-            output=file_output,
-            score=score
-        )
-
+        submission.language = language
+        submission.file_name = file_name
+        submission.output = submission_output
+        submission.score = submission_score
+        
+        submission.save(update_fields=['language', 'file_name', 'output', 'score'])
+        
+        check_competition_best(competition, problem, submission_score, user, user_team)
     else:
-        submission = Submission.objects.create(
-            problem=problem,
-            team=user_team,
-            user=user,
-            language="",
-            file_name="",
-            output=None,
-            score=0
-        )
-        
-        submission_dir = create_user_dir(code, user, problem.name, user_team, submission, subjective=problem.subjective)
-
-        submitted_files = store_submissions(file_paths, submission_dir)
-        
+    
         Notification.objects.create(
             user=user,
             header='Your Submission',
-            body=f'Your file you uploaded has been successfully submitted.',
+            body='The file you uploaded has been successfully submitted.',
         )
         
         notify_admin_submission(competition, user, user_team, problem)
+        
 
-      
-  
+
+   
