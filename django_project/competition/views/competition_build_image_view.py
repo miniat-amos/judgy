024cd6b26189fbe2c93f5510b435566e236b67bb
Slava <yaroslavborysyuk1@gmail.com>
2026-04-22 20:01:42 -0400

    fix: New name for celery image task
    I would like for this file to be removed soon. Either that or make it just a home screen button and not a per comp button.

--- django_project/competition/views/competition_build_image_view.py
+++ django_project/competition/views/competition_build_image_view.py
@@ -1,14 +1,14 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import user_passes_test
-from ..tasks import create_images_task
+from ..tasks import celery_build_judgy_images
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def build_image(request, code):
    if request.method == "POST":
        try:
-           result = create_images_task.delay(code)
+           result = celery_build_judgy_images.delay()
            return JsonResponse({"status": "started", "task_id": result.id}, status=200)
        except Exception as e:
            return JsonResponse({"error": str(e)}, status=500)
