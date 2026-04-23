139b6f08e40cd592f7a2ad5ef511c4187332c37c
Slava <yaroslavborysyuk1@gmail.com>
2026-04-22 20:35:12 -0400

    refactor: Just pulled logic from a utils func to this view

--- django_project/competition/views/problems_update_view.py
+++ django_project/competition/views/problems_update_view.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import redirect, get_object_or_404
 from competition.forms import ProblemForm
 from competition.models import Competition
-from competition.utils import create_problem
+from competition.utils import create_problem_dir
 
 @user_passes_test(lambda u: u.is_superuser)
 def problems_update_view(request, code):
@@ -16,16 +16,31 @@ def problems_update_view(request, code):
             problem.save()
 
             description = request.FILES.get('description')
-            judge_py = request.FILES.get('judge_py')
+            judging_program = request.FILES.get('judge_py')
             other_files = request.FILES.getlist('other_files')
 
-            dist = [
+            dist_files = [
                 key[len('distribute['):-1]
                 for key, val in request.POST.items()
                 if key.startswith('distribute[')
             ]
+            
+            all_files = []
 
-            create_problem(code, problem.name, description, judge_py, other_files, dist)
+            if description:
+                all_files.append(description)
+
+            if judging_program:
+                all_files.append(judging_program)
+
+            all_files.extend(other_files)
+
+            selected_dist_files = [
+                file for file in all_files
+                if file.name in dist_files
+            ]
+
+            create_problem_dir(code, problem.name, description, judging_program, other_files, selected_dist_files)
      
             return redirect('competition:competition_code', code=competition.code)
         else:
